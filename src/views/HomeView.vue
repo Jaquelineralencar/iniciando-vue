3ac6037466b396c86c8d371a7f5d8cdfcc4b440c
Jaqueline Alencar <111159660+Jaquelineralencar@users.noreply.github.com>
2022-11-17 21:08:27 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+<Banner />
+  <div class="main-container">
+   <h1>Monte o seu burguer: </h1>
+   <burgerform />
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import burgerform from "../components/burgerform.vue"
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    burgerform,
+  }
+}
+</script>
